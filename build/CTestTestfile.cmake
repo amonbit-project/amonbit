c5ac1d57f9393afe169773d8b06e9f93d51033dc
MoonX.Linux <agastronics@gmail.com>
2023-03-29 22:29:35 +0100

    Setting up project

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antlion/Projects/amonbit
-# Build directory: /home/antlion/Projects/amonbit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
