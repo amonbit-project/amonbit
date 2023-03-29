e02046938ebb99605f6d5b62204d6e1b0515bc68
AGASTRONICS <agastronics@gmail.com>
2023-03-29 12:10:01 +0100

    Remove build folder and main.cpp from root-dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antlion/Projects
-# Build directory: /home/antlion/Projects/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
